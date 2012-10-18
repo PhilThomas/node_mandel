3ac0dcf9a3adf3e72e411f9dc73161ea3bd88fac
Phil Thomas <phil_thomas@pacbell.net>
2012-10-18 04:41:55 -0400

    Changed some stuff

--- Test.py
+++ Test.py
@@ -1,2 +1,7 @@
-str = raw_input("Please enter a number:")
-print "You entered", str
+#!/usr/bin/env python
+str = raw_input("Please enter a number: ")
+try:
+    i = int(str)
+    print "You entered", str
+except ValueError:
+    print "What??"

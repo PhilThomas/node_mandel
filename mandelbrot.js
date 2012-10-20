519696ddf51f9ceba800490c5e820abe7fad88fa
Phil Thomas <phil_thomas@pacbell.net>
2012-10-20 02:32:34 -0400

    client can zoom on click

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var spawn = require("child_process").spawn;
 var parse = require('url').parse;
+var fs = require('fs');
 
 var PORT = process.env.PORT;
 var IP = process.env.IP;
@@ -21,41 +22,26 @@ function mandel(x, y, k)
     return k;
 }
 
-function handleRoot(url, request, response)
+function sendFile(filename, contentType)
 {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    /*jshint multistr:true */
-    response.end('\
-    <html>\n\
-    <head>\n\
-    <script language="JavaScript">\n\
-    function point_it(event){\n\
-        var mandel = document.getElementById("mandel");\n\
-        var pos_x = event.offsetX?(event.offsetX):event.pageX-mandel.offsetLeft;\n\
-        var pos_y = event.offsetY?(event.offsetY):event.pageY-mandel.offsetTop;\n\
-        var src = "mandelbrot.png?pos_x="+pos_x+"&pos_y="+pos_y;\n\
-        mandel.src = src;\n\
-    }\n\
-    </script>\n\
-    </head>\n\
-    <body>\n\
-    <div id="mandel" onclick="point_it(event)">\n\
-    <img src="mandelbrot.png"/>\n\
-    </div>\n\
-    <div id="text"/>\n\
-    </body>\n\
-    </html>\n');
-    /*jshint multistr:false */
+    return function(url, request, response)
+    {
+        response.writeHead(200, {'Content-Type': contentType});
+        fs.readFile(filename, function(err, data) {
+            if(err) throw err;
+            response.end(data);
+        });
+    }
 }
 
 function handleMandelbrot(url, request, response)
 {
-    var width = parseInt(url.query.width) || 640;
-    var height = parseInt(url.query.height) || 640;
-    var cx = parseFloat(url.query.cx) || -1.0;
-    var cy = parseFloat(url.query.cy) || 0.0;
-    var scale = parseFloat(url.query.scale) || 3.0;
-    var k = parseInt(url.query.k) || (1<<12);
+    var width = parseInt(url.query.width, 10);
+    var height = parseInt(url.query.height, 10);
+    var cx = parseFloat(url.query.cx);
+    var cy = parseFloat(url.query.cy);
+    var scale = parseFloat(url.query.scale);
+    var k = parseInt(url.query.k, 10);
     
     var buf = new Buffer(width*height*3);
     
@@ -68,9 +54,9 @@ function handleMandelbrot(url, request, response)
     for(var y = 0; y < height; y++) {
         for(var x = 0; x < width; x++) {
             var c = ((1<<24)-1) * (mandel(x0+x*dx, y0-y*dy, k) / k);
-            buf[i++] = ~((c >> 0) & 0xff);
-            buf[i++] = ~((c >> 8) & 0xff);
-            buf[i++] = ~((c >> 16) & 0xff);
+            buf[i++] = ((c >> 0) & 0xff);
+            buf[i++] = ((c >> 8) & 0xff);
+            buf[i++] = ((c >> 16) & 0xff);
         }
     }
     
@@ -94,14 +80,15 @@ function handleMandelbrot(url, request, response)
 }
 
 var handlers = {
-    '/': handleRoot,
+    '/': sendFile('index.html', 'text/html'),
+    '/mandel.js': sendFile('mandel.js', 'text/javascript'),
     '/mandelbrot.png': handleMandelbrot
 };
     
 http.createServer(function (request, response) {
     var url = parse(request.url, true);
-    var handler = handlers[url.pathname];
-    if(handler) {
+    if(url.pathname in handlers) {
+        var handler = handlers[url.pathname];
         handler(url, request, response);
     }
     else {
@@ -109,5 +96,3 @@ http.createServer(function (request, response) {
         response.end('Oh Snap! ' + request.url);
     }
 }).listen(PORT, IP);
-
-//console.log('Server running at http://'+IP+':'+PORT+'/');
